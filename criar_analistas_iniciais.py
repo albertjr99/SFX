826329d4aac633bd9f0c5ad252e6d5123133bb51
Albert Projects <aicsj@hotmail.com>
2025-07-03 14:15:56 -0300

    Update criar_analistas_iniciais.py

--- criar_analistas_iniciais.py
+++ criar_analistas_iniciais.py
@@ -3,22 +3,13 @@ from werkzeug.security import generate_password_hash
 
 analistas = [
     # Presencial
-    {"nome": "Shirlene", "email": "shirlene@stc.com", "modalidade": "presencial"},
-    {"nome": "Aline", "email": "aline@stc.com", "modalidade": "presencial"},
-    {"nome": "Mariana", "email": "mariana@stc.com", "modalidade": "presencial"},
-    {"nome": "Ana Lucia", "email": "analucia@stc.com", "modalidade": "presencial"},
-    {"nome": "Caroline", "email": "caroline@stc.com", "modalidade": "presencial"},
-    {"nome": "Flávia", "email": "flavia@stc.com", "modalidade": "presencial"},
+    {"nome": "Lucas",  "email": "lucas@stc.com",  "modalidade": "presencial"},
+    {"nome": "Albert", "email": "albert@stc.com", "modalidade": "presencial"},
+    {"nome": "Eric",   "email": "eric@stc.com",   "modalidade": "presencial"},
+    {"nome": "Ana",    "email": "ana@stc.com",    "modalidade": "presencial"},
 
     # Teletrabalho
-    {"nome": "Antonio Henrique", "email": "antonio@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Fabriciano", "email": "fabriciano@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Talmom", "email": "talmom@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Cristina", "email": "cristina@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Alexandre", "email": "alexandre@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Gileade", "email": "gileade@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Silvana", "email": "silvana@stc.com", "modalidade": "teletrabalho"},
-    {"nome": "Soraya", "email": "soraya@stc.com", "modalidade": "teletrabalho"},
+    {"nome": "Almino", "email": "almino@stc.com", "modalidade": "teletrabalho"},
 ]
 
 with app.app_context():
@@ -34,4 +25,3 @@ with app.app_context():
 
     db.session.commit()
     print("✅ Analistas cadastrados com sucesso!")
-
